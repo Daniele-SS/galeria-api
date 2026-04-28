4240acb05622cf33b3f96e7ec161d1f0b324ec4f
Daniele-SS <dev.daniele@outlook.com>
2026-04-28 11:11:59 -0300

    feat: retorna todas as fotos de cachorros que estão disponíveis na API conforme a raça digitada pelo usuário

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 'use strict'
 
 async function getUrlImagens(raca) {
-    const url = `https://dog.ceo/api/breed/${raca}/images`
-    const response = await fetch(url) //Como irei navegar na API que retorna as imagens, usarei o async e await na função
-    const data = await response.json()
+    const url       = `https://dog.ceo/api/breed/${raca}/images`
+    const response  = await fetch(url) //Como irei navegar na API que retorna as imagens, usarei o async e await na função
+    const data      = await response.json()
     return data.message
 }
 
-function preencherGaleria() {
-    const raca = document.getElementById('raca').value
-    const urlImagens = getUrlImagens(raca)
+function criarFoto(urlFoto){
+    const foto      = document.createElement('img')
+    foto.src        = urlFoto
+    foto.className  = 'foto'
+    return foto
+}
 
-    const fotos = urlImagens.map(criarFoto)
+
+async function preencherGaleria() {
+    const galeria       = document.getElementById('container-galeria')
+    const raca          = document.getElementById('raca').value
+    const urlImagens    = await getUrlImagens(raca)
+
+    const fotos = urlImagens.map(criarFoto) //Armaneza as fotos dos cachorros. O .map() funciona como um callback
+
+    galeria.replaceChildren(...fotos)
 }
 
 document.getElementById('pesquisar').addEventListener('click', preencherGaleria)
